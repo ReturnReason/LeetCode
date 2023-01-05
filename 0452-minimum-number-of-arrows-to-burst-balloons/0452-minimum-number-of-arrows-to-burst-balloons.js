a5833ddb40384c16825d27bf18ef8f632b0a1760
REASON <reasonz@daum.net>
2023-01-05 21:22:08 +0900

    Time: 528 ms (8.96%), Space: 73.8 MB (55.17%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    points.sort((a, b) => a[0] - b[0]);
+    
+    let cnt = 0;
+    let overlap;    
+    
+    
+    points.forEach((p) => {
+        if(!overlap || overlap[1] < p[0] ){
+            cnt++;
+            overlap = p;
+        } else {
+            const a = Math.max(overlap[0], p[0]);
+            const b = Math.min(overlap[1], p[1]);
+            overlap = [a, b];
+        }
+    })
+    
+    return cnt;
+};
