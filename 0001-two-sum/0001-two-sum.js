44b8443741bb8cc6038aee83993a700e4064521a
REASON <reasonz@daum.net>
2023-01-11 17:46:20 +0900

    Time: 72 ms (89.66%), Space: 44.7 MB (9.99%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const ps = {};
+    const ret = [];
+    
+    for(let i = 0; i < nums.length; i++){
+        const findTargat = target - nums[i];
+        
+        if(ps[findTargat]){
+        ret.push(nums.indexOf(findTargat));
+         ret.push(i);
+        }
+        ps[nums[i]] = (ps[nums[i]] || 0 ) + 1;
+        
+    }
+    
+    return ret;
+};
