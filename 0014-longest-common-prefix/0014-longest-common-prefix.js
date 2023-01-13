74e21b7fcb9129362adffcb1f81edded19abab26
REASON <reasonz@daum.net>
2023-01-13 22:58:46 +0900

    Time: 67 ms (86.61%), Space: 42.3 MB (69.84%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 1) return strs[0]
+    
+    strs.sort();
+    
+    if(!strs[0].length) return '';
+    
+    let prefix = '';
+    for(let i = 0; i < strs[0].length; i++){
+        if(strs[0] === strs[strs.length - 1]) return strs[0];
+        if(strs[0][i] === strs[strs.length - 1][i]){
+            prefix += strs[0][i];
+        } else break;
+    }
+    
+    return prefix;
+};
