bcb1c51a82cdc4ba4b4ece7b3ecb82bf6b7c690a
REASON <reasonz@daum.net>
2023-01-15 16:26:21 +0900

    Time: 56 ms (96.44%), Space: 42.2 MB (31.68%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    if(nums.length === 1 && nums[0] === val) return nums.splice(0);
+        
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while(start < end){
+        if(nums[start] === val){
+            nums[start] = nums[end];
+            end--;
+        } else {
+            start++;
+        } 
+    }
+    
+    if(nums[start] === val) {
+        nums.splice(start);
+    } else {
+        nums.splice(start + 1);
+    }
+};
