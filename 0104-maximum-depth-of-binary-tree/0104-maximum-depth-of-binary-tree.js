3ef14304366e053a1af01d498306a9013d050de3
REASON <reasonz@daum.net>
2023-02-02 21:35:52 +0900

    Time: 78 ms (48.87%), Space: 45.5 MB (19.02%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if(!root) return 0;
+  
+  let [left, right] = [1, 1];
+  
+  left += maxDepth(root.left);
+  right += maxDepth(root.right);
+  
+  return Math.max(left, right);
+};
